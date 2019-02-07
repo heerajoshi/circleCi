54a1fe943d95c833d942c938a992cbbe5e0aaadc
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2019-02-08 00:12:33 +0530

    Added some more test and change the circle config file now circleci have the coverge report

--- test/calculatNumTest.js
+++ test/calculatNumTest.js
@@ -5,4 +5,10 @@ describe("add", function() {
   it("should return the addition of two number", function() {
     assert.deepEqual(add(2, 3), 5);
   });
+  it("should return sub of two num",function(){
+    assert.equal(sub(9,5),4);
+  })
+  it("should return mul of two number",function(){
+    assert.equal(mul(5,2),10)
+  })
 });
