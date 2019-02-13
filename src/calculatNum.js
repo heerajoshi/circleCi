2d08a3127ccfdeeb15de0ef5cdcf2e7bac04ba96
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2019-02-13 15:51:57 +0530

    modify package.json

--- src/calculatNum.js
+++ src/calculatNum.js
@@ -1,13 +1,23 @@
-const add = function(a,b){
-  return a+b;
+const add = function(a, b) {
+  return a + b;
 };
 
-const sub = function(a,b){
-  return a-b;
+const sub = function(a, b) {
+  return a - b;
+};
+
+const mul = function(a, b) {
+  return a * b;
+};
+
+class Calculate {
+  constructor() {}
+  add(a, b) {
+    return a + b;
+  }
+  sub(a, b) {
+    return a - b;
+  }
 }
 
-const mul = function(a,b){
-  return a*b;
-}
-
-module.exports = {add,sub,mul};
+module.exports = { add, sub, mul , Calculate};
--- test/calculatNumTest.js
+++ test/calculatNumTest.js
@@ -1,14 +1,21 @@
 const assert = require("assert");
-const { add, sub, mul } = require("../src/calculatNum");
+const { add, sub, mul, Calculate } = require("../src/calculatNum");
+const calculater = new Calculate();
 
 describe("add", function() {
   it("should return the addition of two number", function() {
     assert.deepEqual(add(2, 3), 5);
   });
-  it("should return sub of two num",function(){
-    assert.equal(sub(9,5),4);
-  })
-  it("should return mul of two number",function(){
-    assert.equal(mul(5,2),10)
-  })
+  it("should return sub of two num", function() {
+    assert.equal(sub(9, 5), 4);
+  });
+  it("should return mul of two number", function() {
+    assert.equal(mul(5, 2), 10);
+  });
+});
+
+describe("calculater", function() {
+  it("it should return the value after calculate the no accrodind to number", function() {
+    assert.deepEqual(calculater.add(3, 5), 8);
+  });
 });
